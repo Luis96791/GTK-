d40afbd272911d2f5113932ca734aeb1826c40e2
Luis96791 <luis.fajardozap@hotmail.com>
2017-05-26 12:09:44 -0600

    Torres Hanoi

--- HanoiTowers/main.cpp
+++ HanoiTowers/main.cpp
@@ -2,28 +2,36 @@
 
 using namespace std;
 
-void hanoi(int numero, int torre1, int torre2, int torre3)
-{
-    if(numero > 1)
-    {
-        hanoi((numero - 1), torre1, torre3, torre2);
-        cout<<"Moviendo el disco "<<torre1<<" a "<<torre3<<endl;
-        hanoi((numero - 1), torre2, torre1, torre3);
-    }
+class Hanoi{
 
-    if(numero == 1)
-    {
-        cout<<"Moviendo el disco "<<torre1<<" a "<<torre3<<endl;
-    }
-}
+    public:
+        long int movimientos;
+
+        Hanoi()
+        {
+            movimientos = 0;
+        }
+
+        long int hanoi(int numero, int torre1, int torre2, int torre3)
+        {
+            if(numero > 0)
+            {
+                hanoi((numero - 1), torre1, torre3, torre2);
+                cout<<++movimientos<<". Moviendo el disco de la torre "<<torre1<<" a la torre "<<torre3<<endl;
+                hanoi((numero - 1), torre2, torre1, torre3);
+            }
+        }
+};
 
 int main()
 {
+    Hanoi* torre_hanoi = new Hanoi();
     int numero;
 
     cout<<"Numero de discos: ";
     cin>>numero;
 
-    hanoi(numero, 1, 2, 3);
+    torre_hanoi->hanoi(numero, 1, 2, 3);
+    cout<<"Movimientos: "<<torre_hanoi->movimientos<<endl;
     return 0;
 }
